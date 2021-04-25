50f1906afafb84ee350e6a005fbebb74ca666245
Stevtm <steventmaddison@outlook.com>
2021-04-25 19:48:22 -0400

    add param request

--- server.js
+++ server.js
@@ -38,6 +38,11 @@ function filterByQuery(query, animalsArray) {
 	return filteredResults;
 }
 
+function findById(id, animalsArray) {
+	const result = animalsArray.filter((animal) => animal.id === id)[0];
+	return result;
+}
+
 app.get("/api/animals", (req, res) => {
 	let results = animals;
 
@@ -46,6 +51,11 @@ app.get("/api/animals", (req, res) => {
 	}
 
 	res.json(results);
+});
+
+app.get("/api/animals/:id", (req, res) => {
+	const result = findById(req.params.id, animals);
+	result ? res.json(result) : res.send(404);
 });
 
 app.listen(PORT, () => {
