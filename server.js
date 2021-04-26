335640eae1043ad8fa9f022bd59845b411d571b0
Stevtm <steventmaddison@outlook.com>
2021-04-26 08:00:54 -0400

    add POST method to add new animals

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const { animals } = require("./data/animals.json");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
 	let personalityTraitsArray = [];
@@ -43,6 +50,17 @@ function findById(id, animalsArray) {
 	return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+	const animal = body;
+	animalsArray.push(animal);
+	fs.writeFileSync(
+		path.join(__dirname, "./data/animals.json"),
+		JSON.stringify({ animals: animalsArray }, null, 2)
+	);
+
+	return animal;
+}
+
 app.get("/api/animals", (req, res) => {
 	let results = animals;
 
@@ -56,6 +74,16 @@ app.get("/api/animals", (req, res) => {
 app.get("/api/animals/:id", (req, res) => {
 	const result = findById(req.params.id, animals);
 	result ? res.json(result) : res.send(404);
+});
+
+app.post("/api/animals", (req, res) => {
+	// set id based on what the next index of the array will be
+	req.body.id = animals.length.toString();
+
+	// add animal to json file and animals array
+	const animal = createNewAnimal(req.body, animals);
+
+	res.json(animal);
 });
 
 app.listen(PORT, () => {
